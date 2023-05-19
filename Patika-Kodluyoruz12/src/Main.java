569c5f0329a6e416d6455b69fb70793bf430ecba
MELSANYILMAZ <melsanyilmaz02@gmail.com>
2023-05-19 14:05:39 +0300

    Burçlar
    Girilen ay ve güne göre burcunu belirten kodlama

---
+++ Patika-Kodluyoruz12/src/Main.java
@@ -0,0 +1,64 @@
+/*Koç Burcu : 21 Mart - 20 Nisan
+Boğa Burcu : 21 Nisan - 21 Mayıs
+İkizler Burcu : 22 Mayıs - 22 Haziran
+Yengeç Burcu : 23 Haziran - 22 Temmuz
+Aslan Burcu : 23 Temmuz - 22 Ağustos
+Başak Burcu : 23 Ağustos - 22 Eylül
+Terazi Burcu : 23 Eylül - 22 Ekim
+Akrep Burcu : 23 Ekim - 21 Kasım
+Yay Burcu : 22 Kasım - 21 Aralık
+Oğlak Burcu : 22 Aralık - 21 Ocak
+Kova Burcu : 22 Ocak - 19 Şubat
+Balık Burcu : 20 Şubat - 20 Mart
+Ödev
+Aynı örneği switch-case kullanmadan yapınız*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int month, day;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Doğum ayınızı giriniz: ");
+        month = input.nextInt();
+        System.out.print("Doğum gününüzü giriniz: ");
+        day = input.nextInt();
+
+        String burc = "";
+
+        if (month < 1 || month > 12 || day < 1 || day > 31) {
+            System.out.println("Geçersiz tarih girdiniz!");
+        } else {
+            if ((month == 1 && day >= 22) || (month == 2 && day <= 19)) {
+                burc = "Kova";
+            } else if ((month == 2 && day >= 20) || (month == 3 && day <= 20)) {
+                burc = "Balık";
+            } else if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
+                burc = "Koç";
+            } else if ((month == 4 && day >= 21) || (month == 5 && day <= 21)) {
+                burc = "Boğa";
+            } else if ((month == 5 && day >= 22) || (month == 6 && day <= 22)) {
+                burc = "İkizler";
+            } else if ((month == 6 && day >= 23) || (month == 7 && day <= 22)) {
+                burc = "Yengeç";
+            } else if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
+                burc = "Aslan";
+            } else if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) {
+                burc = "Başak";
+            } else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
+                burc = "Terazi";
+            } else if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) {
+                burc = "Akrep";
+            } else if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) {
+                burc = "Yay";
+            } else if ((month == 12 && day >= 22) || (month == 1 && day <= 21)) {
+                burc = "Oğlak";
+            }
+
+            System.out.println("Burcunuz: " + burc);
+        }
+
+        input.close();
+    }
+}
